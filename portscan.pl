91f8e84729acba3d51b47fcbe98c7f2dfb2e29d1
William Coffey <WilliamCoffeyTri@gmail.com>
2018-02-25 23:10:11 -0500

    Added parsing to identify target ports.

--- portscan.pl
+++ portscan.pl
@@ -43,7 +43,32 @@ sub parseInputAsAddress {
 }
 
 
+sub parsePorts {
+	my @ports = split(/,/, shift);
+
+	my @compiledPorts = ();
+	
+	foreach my $port (@ports) {
+		if ($port =~ /-/) {
+			my @range = split(/-/, $port);
+			
+			for my $i ($range[0] .. $range[1]) {
+				push @compiledPorts, $i;
+			}
+		}
+		else {
+			push @compiledPorts, $port;
+		}
+	}
+	
+	return @compiledPorts;
+}
+
+
 sub scanTargets {
+	my $ports_ref = shift;
+	my @ports = @$ports_ref;
+	
 	my $addr_ref = shift;
 	my @addresses = @$addr_ref;
 	
@@ -52,11 +77,13 @@ sub scanTargets {
 	my @lower_bound = @$lower_bound_ref;
 	my @upper_bound = @$upper_bound_ref;
 	
+	print "Ports: " . join(", ", @ports) . "\n";
+	
 	for my $a ($lower_bound[0] .. $upper_bound[0]) {
 		for my $b ($lower_bound[1] .. $upper_bound[1]) {
 			for my $c ($lower_bound[2] .. $upper_bound[2]) {
 				for my $d ($lower_bound[3] .. $upper_bound[3]) {
-					print "$a.$b.$c.$d\n";
+					#print "$a.$b.$c.$d\n";
 				}
 			}
 		}
@@ -66,6 +93,7 @@ sub scanTargets {
 
 if (scalar(@ARGV) == 2) {
 	my  @target = ();
+	my  @ports = ();
 	
 	if ($ARGV[0] =~ (/^([0-9]{1,3}.){3}[0-9]{1,3}-([0-9]{1,3}.){3}[0-9]{1,3}$/)) {
 		@target = parseInputAsRange($ARGV[0]);
@@ -78,12 +106,15 @@ if (scalar(@ARGV) == 2) {
 	}
 	else { usage(); }
 	
+	if ($ARGV[1] =~ (/^( *[0-9]{1,4}(-[0-9]{1,4})?,)*( *[0-9]{1,4}(-[0-9]{1,4})?)$/)) {
+		@ports = parsePorts($ARGV[1]);
+	}
+	else { usage(); }
 	
 	if (!@target) { usage(); }
 
-	scanTargets(\@target);
+	scanTargets(\@ports, \@target);
 }
-
 else {
 	usage();
 }
